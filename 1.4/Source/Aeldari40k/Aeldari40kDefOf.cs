b0afcc019bee72428cfdc85269e60125e4a183e3
Phonicmas <jkskovlund@gmail.com>
2024-02-07 07:56:16 +0100

    Currently Made

---
+++ 1.4/Source/Aeldari40k/Aeldari40kDefOf.cs
@@ -0,0 +1,15 @@
+﻿using RimWorld;
+using Verse;
+
+namespace Abhumans40k
+{
+    [DefOf]
+    public static class Aeldari40kDefOf
+    {
+
+        static Aeldari40kDefOf()
+        {
+            DefOfHelper.EnsureInitializedInCtor(typeof(Aeldari40kDefOf));
+        }
+    }
+}
---
+++ 1.4/Source/Aeldari40k/Aeldari40kMod.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using Verse;
+
+
+namespace Abhumans40k
+{
+    public class Aeldari40kMod : Mod
+    {
+        public static Harmony harmony;
+
+        public Aeldari40kMod(ModContentPack content) : base(content)
+        {
+            harmony = new Harmony("Aeldari40k.Mod");
+            harmony.PatchAll();
+        }
+    }
+}
