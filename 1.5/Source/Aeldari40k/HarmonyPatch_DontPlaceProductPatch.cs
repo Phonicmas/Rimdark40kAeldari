a3475249ced7c377d6c71876ba1adb91f6df77fb
Phonicmas <jkskovlund@gmail.com>
2024-05-10 22:37:39 +0200

    Fixes

---
+++ 1.5/Source/Aeldari40k/HarmonyPatch_DontPlaceProductPatch.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using Verse;
+
+namespace Aeldari40k
+{
+    [HarmonyPatch(typeof(GenRecipe), "MakeRecipeProducts")]
+    public class DontPlaceProductPatch
+    {
+        public static bool Prefix(ref IEnumerable<Thing> __result, RecipeDef recipeDef)
+        {
+            Log.Message(recipeDef);
+            Log.Message(recipeDef.HasModExtension<DefModExtension_WraithboneCreatedStuff>());
+            if (recipeDef.HasModExtension<DefModExtension_WraithboneCreatedStuff>())
+            {
+				__result = new List<Thing>();
+                return false;
+            }
+            return true;
+        }
+    }
+}
